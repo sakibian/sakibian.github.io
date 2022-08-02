b9b18a7842f6b64c30f0893cab3290e4e6642460
sakib192 <sakib.works@gmail.com>
2022-08-02 11:33:57 +0600

    Testing gh-pages

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Start Bootstrap - Resume v7.0.5 (https://sakibian.github.io/sakibian.github.io)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/BakibillahSakib/blob/master/LICENSE)
 */
 //
 // Scripts
